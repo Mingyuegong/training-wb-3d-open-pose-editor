7b028d1359e58d2f742da22f8cb8c1c2b2c7e0e2
nonnonstop <42905588+nonnonstop@users.noreply.github.com>
2023-03-28 13:14:00 +0900

    Update compiled js

--- javascript/index.js
+++ javascript/index.js
@@ -7,7 +7,7 @@
   };
   onUiLoaded(async () => {
     console.log("sd-webui-3d-open-pose-editor: onUiLoaded");
-    const { Main: o } = await import("./lazy/main.js");
+    const { Main: o } = await import("./lazy/main.js?c7969a7a");
     window.openpose3dglobal = await o();
   }), onUiTabChange(() => {
     var e;
